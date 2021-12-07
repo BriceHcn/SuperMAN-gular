46b73441dd279cde05225103c4826e3a00724760
JulienGWebDev <julienguinhut@hotmail.com>
2021-12-07 10:56:32 +0100

    Update biography.ts

--- superMangular/src/app/data/biography.ts
+++ superMangular/src/app/data/biography.ts
@@ -1,5 +1,13 @@
 export class biography {
 
+  "full-name"!: string;
+  "alter-egos"!: string;
+  aliases!: Array<string>;
+  "place-of-birth"!:string;
+  "first-appearance"!:string;
+  publisher!:string;
+  alignment!:string;
+
 
 constructor(){
 }
