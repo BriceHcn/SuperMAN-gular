508ca0961f7ba2d84c22061e4af2dcba4847ca13
BrSkat <brice.hechon@isen.yncrea.fr>
2021-12-07 14:08:22 +0100

    Revert "service cherche le numero donné"
    This reverts commit 19154132d7d10be48478bd7b9d16507f165a989d.

--- superMangular/src/app/services/data-service.service.ts
+++ superMangular/src/app/services/data-service.service.ts
@@ -16,6 +16,6 @@ export class DataServiceService {
 
 
   getSuperhero(id:number): Observable<superhero> {
-    return this.http.get<superhero>(this.baseUrl + id.toString());
+    return this.http.get<superhero>(this.baseUrl + "201");
   }
 }
