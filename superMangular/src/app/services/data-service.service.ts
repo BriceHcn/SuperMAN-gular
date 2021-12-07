937a4406403bf3a1be74b9bb067142fde01d3545
BrSkat <brice.hechon@isen.yncrea.fr>
2021-12-07 11:09:34 +0100

    Update data-service.service.ts

--- superMangular/src/app/services/data-service.service.ts
+++ superMangular/src/app/services/data-service.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, delay, map, tap } from 'rxjs/operators';
+import { superhero } from '../data/superhero';
+import { environment } from '../../environments/environment'
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataServiceService {
+  //baseUrl: string = environment.baseUrl;
+  baseUrl: string = "https://superheroapi.com/api/1747455068784069/";
+  constructor( private http: HttpClient) { }
 
-  constructor() { }
+
+  getSuperhero(id:number): Observable<superhero> {
+    return this.http.get<superhero>(this.baseUrl + id.toString());
+  }
 }
