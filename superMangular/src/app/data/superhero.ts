920caa7121a7093a23ce429e8ad309cee9b693d1
JulienGWebDev <julienguinhut@hotmail.com>
2021-12-07 10:22:48 +0100

    Update superhero.ts

--- superMangular/src/app/data/superhero.ts
+++ superMangular/src/app/data/superhero.ts
@@ -1,2 +1,28 @@
 
-export class superhero { }
+export class superhero {
+
+
+  id: number;
+  name: string;
+  powerstats: powerstats;
+  biography: biography;
+  appearance:appearance;
+  work:work;
+  connections:connections;
+  image:image;
+
+
+  constructor(id: number, name: string, powerstats: powerstats, biography:biography, appearance:appearance, work:work, connections:connections, image:image) {
+
+    this.id = id;
+    this.name = name;
+    this.powerstats = powerstats;
+    this.biography = biography;
+    this.appearance = appearance;
+    this.work = work;
+    this.connections = connections;
+    this.image = image;
+
+
+}
+ }
