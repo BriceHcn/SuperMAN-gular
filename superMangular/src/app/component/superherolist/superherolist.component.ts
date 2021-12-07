4bb7d2e6cb3661cce04f65faa2be9660820acdc2
BrSkat <brice.hechon@isen.yncrea.fr>
2021-12-07 17:50:56 +0100

    le score marche vraiment bien

--- superMangular/src/app/component/superherolist/superherolist.component.ts
+++ superMangular/src/app/component/superherolist/superherolist.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
 import { superhero } from 'src/app/data/superhero';
 import { Subscription } from 'rxjs';
 import { DataServiceService } from 'src/app/services/data-service.service';
@@ -28,30 +28,54 @@ export class SuperherolistComponent implements OnInit {
         this.hideMe=true;}
       ,(error) => {console.log("error");}
       ,() => {console.log("bravo");}
-  );
+    );
 
-  this.subscription2 = this.DataService.getSuperhero().subscribe(
-    (data2:superhero) =>{
+    this.subscription2 = this.DataService.getSuperhero().subscribe(
+      (data2:superhero) =>{
 
-      this.myHero2 = data2;}
-    ,(error) => {console.log("error");}
-    ,() => {console.log("bravo");}
-);
+        this.myHero2 = data2;}
+      ,(error) => {console.log("error");}
+      ,() => {console.log("bravo");}
+    );
+
+    this.GameService.scoreObs.subscribe((value: number) => {
+      console.log("caca");
+
+      this.score=value;
+
+      this.subscription2 = this.DataService.getSuperhero().subscribe(
+        (data2:superhero) =>{
+
+          this.myHero2 = data2;}
+        ,(error) => {console.log("error");}
+        ,() => {console.log("bravo");}
+      );
+
+      this.subscription = this.DataService.getSuperhero().subscribe(
+        (data1:superhero) =>{
+          this.myHero1 = data1;
+          this.hideMe=true;}
+        ,(error) => {console.log("error");}
+        ,() => {console.log("bravo");}
+      );
 
 
+    }
+    );
   }
+
   actionLeft(){
     console.log("les deux super etait "+this.myHero1.name +" & "+this.myHero2.name);
     console.log("jai choisi le super gauche : "+this.myHero1.name);
     this.GameService.compareHero(this.myHero2,this.myHero1,1);
-    this.ngOnInit();
+
   }
 
   actionRight(){
     console.log("les deux super etait "+this.myHero2.name +" & "+this.myHero2.name);
     console.log("jai choisi le super droite : "+this.myHero2.name);
     this.GameService.compareHero(this.myHero1,this.myHero2,1);
-    this.ngOnInit();
+
   }
 
   NgOnDestroy(){
