a8a9e3fda955b507c7f4318f8029ad2027713a5d
JulienGWebDev <julienguinhut@hotmail.com>
2021-12-07 11:29:18 +0100

    Update app.module.ts

--- superMangular/src/app/app.module.ts
+++ superMangular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { SuperheroComponent } from './component/superhero/superhero.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
