bc7a3149629dc5b85fc1fadd4e6e272afcb8e211
BrSkat <brice.hechon@isen.yncrea.fr>
2022-01-04 19:11:18 +0100

    mon dernier mot

--- superMangular/server.js
+++ superMangular/server.js
@@ -1,11 +1,20 @@
 //Install express server
+var proxy = require('express-http-proxy');
 const express = require('express');
 const path = require('path');
+const url = require('url');
+
+// New hostname+path as specified by question:
+const apiProxy = proxy('https://superheroapi.com/api/', {
+    forwardPath: req => url.parse(req.baseUrl).path
+});
 
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/superMangular'));
+
+app.use('/api/*', apiProxy);
 
 app.get('/*', function(req, res) {
 
