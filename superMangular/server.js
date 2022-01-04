2033aca7fe7da81489cc652c3287e5f78dbae0bf
BrSkat <brice.hechon@isen.yncrea.fr>
2022-01-04 16:12:25 +0100

    test

--- superMangular/server.js
+++ superMangular/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/superMangular'));
+app.use(express.static(__dirname + './dist/superMangular'));
 
 app.get('/*', function(req, res) {
 
